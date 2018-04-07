cad4e14d5f5cad7e7482de8abfdc4197fa315cc8
Yuhang Lin <ylin19@uvm.edu>
2018-04-07 15:18:36 -0400

    Delete comment

--- templates/found.php
+++ templates/found.php
@@ -58,15 +58,7 @@ include("top.php");
                         <input class="form-control input-lg" id="inputlg" type="text">
                     </div>
                 </div>
-
-
-                <!--   <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-10">
-                      <div class="checkbox">
-                        <label><input type="checkbox"> Remember me</label>
-                      </div>
-                    </div> -->
-
+                
             </div>
             <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10">
